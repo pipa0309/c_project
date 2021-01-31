8aaaf79e1b2325be0950b078aa50cada2e4e360f
Evgeny <melatonin24@yandex.ru>
2021-01-31 19:13:56 +0300

    revers

--- arr2.0/Source.c
+++ arr2.0/Source.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define N 5
+#define N 12
 
 void printArray(int* pBegin, int count)
 {
@@ -20,21 +20,48 @@ void sequence(int* array, int count)
 	}
 }
 
+void swap(int* a, int* b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void testSwap()
+{
+	printf("start test swap\n");
+	int x = 0;
+	int y = 1;
+	printf("x = %d; y = %d\n", x, y);
+	swap(&x, &y);
+	printf("x = %d; y = %d\n", x, y);
+}
+
 //nado dopisat' ))
 void revers(int* array, int count)
 {
-	int i;
-	for (int i = 0; i < count; ++i)
-	r = count - i - 1;
-	printf("%d", r);
+	int j = count - 1;
+	for (int i = 0; i < j; ++i, --j)
+	{
+		int* first = &array[i];
+		int* last = &array[j]; 
+		swap(first, last);
+	}
+}
+
+void testRevers()
+{
+	printf("start test revers\n");
+	int a[N] = { 0 };
+	sequence(a, N);
+	printArray(a, N);
+	revers(a, N);
+	printArray(a, N);
 }
 
 int main()
 {
-	int array[N] = {0};
-	printArray(array, N);
-	sequence(array, N);
-	printArray(array, N);
-	revers(array, N);
+	testRevers();
+	//testSwap();
 	return 0;
 }
